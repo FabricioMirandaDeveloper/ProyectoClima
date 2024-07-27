637b3319ccbe0b30eea4b870b2737f2ca2cb886e
Fabricio Miranda <mirandaquezadafabriciojoel@gmail.com>
2024-07-26 23:40:21 -0500

    Mejora el diseño y la funcionalidad de la vista del pronóstico del clima; ajusta el componente WeatherInfo y ForecastInfo

--- App Clima/src/components/ForecastInfo.tsx
+++ App Clima/src/components/ForecastInfo.tsx
@@ -5,23 +5,37 @@ interface ForecastInfoProps {
 }
 
 export const ForecastInfo = ({ forecastData }: ForecastInfoProps) => {
+    const hasData = forecastData.length > 0;
     return (
-        <div className="mt-4">
-            <h3 className="text-xl font-semibold mb-2">Forecast</h3>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-                {forecastData.map((item) => (
-                    <div key={item.date} className="bg-white p-4 rounded-lg shadow-md">
-                        <h4 className="text-lg font-semibold">{new Date(item.date).toLocaleDateString()}</h4>
-                        <p className="text-lg">Temperature: {item.temp}°C</p>
-                        <p className="text-lg">Description: {item.description}</p>
-                        <img
-                            src={`https://openweathermap.org/img/wn/${item.icon}.png`}
-                            alt="Weather Icon"
-                            className="mt-2"
-                        />
-                    </div>
-                ))}
+        <div className="text-center my-5">
+            {hasData && (
+                <div className="flex flex-col items-center">
+                <h3 className="text-lg font-semibold mb-2">Pronostico</h3>
+                <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
+                    {forecastData.map((item) => (
+                        <div
+                            key={item.date}
+                            className="bg-[#313050] text-white p-4 rounded-lg w-full sm:w-11/12 mx-auto flex flex-col items-center"
+                        >
+                            <h4 className="text-md sm:text-lg font-semibold mb-2">
+                                {new Date(item.date).toLocaleDateString()}
+                            </h4>
+                            <p className="text-sm sm:text-base md:text-lg mb-1">
+                                Temperature: {item.temp}°C
+                            </p>
+                            <p className="text-sm sm:text-base md:text-lg mb-2">
+                                Description: {item.description}
+                            </p>
+                            <img
+                                src={`https://openweathermap.org/img/wn/${item.icon}.png`}
+                                alt="Weather Icon"
+                                className="mt-2 w-12 h-12 sm:w-16 sm:h-16 md:w-20 md:h-20"
+                            />
+                        </div>
+                    ))}
+                </div>
             </div>
+            )}
         </div>
     );
 };
--- App Clima/src/components/WeatherInfo.tsx
+++ App Clima/src/components/WeatherInfo.tsx
@@ -7,14 +7,14 @@ const WeatherInfo = ({weatherData}: WeatherInfoProps) => {
     return (
         <>
         {weatherData && (
-                <div className='mt-4'>
-                    <h3 className="text-xl font-semibold">{weatherData.name}</h3>
-                    <p className="text-lg">Temperature: {weatherData.main.temp}°C</p>
-                    <p className="text-lg">Description: {weatherData.weather[0].description}</p>
+                <div className='mt-4 flex flex-col justify-center items-center bg-[#313050] text-white p-4 rounded-lg shadow-lg w-9/12 mx-auto sm:w-8/12 md:w-7/12 lg:w-6/12'>
+                    <h3 className="text-lg sm:text-xl md:text-2xl lg:text-3xl font-bold mb-1">{weatherData.name}</h3>
+                    <p className="text-sm sm:text-base md:text-lg lg:text-xl mb-2">Temperature: {weatherData.main.temp}°C</p>
+                    <p className="text-sm sm:text-base md:text-lg lg:text-xl mb-3">Description: {weatherData.weather[0].description}</p>
                     <img
                         src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`}
                         alt="Weather Icon"
-                        className="mt-2"
+                        className="mt-2 w-16 h-16 sm:w-24 sm:h-24 md:w-28 md:h-28"
                     />
                 </div>
             )}
